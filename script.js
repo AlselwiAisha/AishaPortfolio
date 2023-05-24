70af0f74869a2d779ac780a55da310fbf717bc96
Aisha Alselwi <123330794+AlselwiAisha@users.noreply.github.com>
2023-05-24 07:41:43 +0300

    Added createWorkCard function to added works containers

--- script.js
+++ script.js
@@ -11,94 +11,132 @@ window.onscroll = () => {
   menu.classList.remove('bx-x');
   nav.classList.remove('open-menu');
 };
+/* -----------------------main projects--------------------*/
 
+const works = [
+  {
+    id: 1,
+    title: 'Tonic',
+    farme: ['CANOPY', 'Back End Dev', 2015],
+    image: './Images/1.png',
+    description_: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    description: ` Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                   when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    type: 1,
+  },
+
+  {
+    id: 2,
+    title: 'Multi-Post Stories',
+    farme: ['FACEBOOK', 'Full Stack Dev', 2015],
+    image: './Images/2.png',
+    description_: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    description: ` Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                   when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                   It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    type: 2,
+  },
+
+  {
+    id: 3,
+    title: 'Tonic',
+    farme: ['FACEBOOK', 'Full Stack Dev', 2015],
+    image: './Images/3.png',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    description1: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    type: 1,
+  },
+
+  {
+    id: 4,
+    title: 'Multi-Post Stories',
+    farme: ['Uber', 'Lead Developer', 2018],
+    image: './Images/4.png',
+    description_: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    description: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
+    type: 2,
+  },
+];
+function createWorkCard() {
+  works.forEach((work) => {
+    const sampleTemplateModal = `
+  <section class="work-container${work.type}">
+  <img src=${work.image} alt="">
+  <div class="work">
+    <h2>${work.title}</h2>
+    <div class="facebook"> <p>${work.farme[0]}</p>
+      <ul>
+         <li>${work.farme[1]}</li>
+         <li>${work.farme[2]}</li>
+      </ul>
+    </div>
+
+  <p class="work-p">${work.description}</p>
+  <ul class="work-list">
+     <li><a href=""> html</a></li>
+     <li class="desktop_link"><a href=""> Ruby on rails</a></li>
+     <li><a href=""> css</a></li>
+     <li><a href=""> javaScript</a></li>
+   </ul>
+   <button class="btn modal-btn" id="${work.id}">See project</button>
+ </div>
+</section>
+`;
+    const element = document.querySelector('.my-works');
+    element.innerHTML += sampleTemplateModal;
+  });
+}
+createWorkCard();
 /* -----------------------popup window-------------------------*/
 
 const openPopButtons = document.querySelectorAll('.modal-btn');
-const overlay = document.createElement('div');
-overlay.setAttribute('id', 'overlay');
-function createModal(id, title, image, description, skills) {
+const overlay = document.querySelector('#overlay');
+function createModal(id, title, image, description, farme) {
   const sampleTemplateModal = `
   <section class="modal-work">
         <div class="header-wrapper">
             <h2>${title}</h2>
-             <a href="#/" id="crossBtnImg">
+             <a href="#" id="crossBtnImg">
                 <img src="./Images/Cancel.png" alt="" class="data-close-button">
             </a>
         </div>
-        <ul class="modal-facebook">
-            <li id="canopy-text">CANOPY</li>
-            <li>Back End Dev</li>
-            <li>2015</li>
-        </ul>
-        <img src="${image}" alt="" class="snap-shot-imagea modal-image">
-        <div class="info-skill-container">
+        <div class="facebook"> <p>${farme[0]}</p>
+           <ul>
+              <li>${farme[1]}</li>
+              <li>${farme[2]}</li>
+           </ul>
+        </div>
+        <img src="${image}" alt="" class="modal-image">
+        <div class="info-skill">
           <p class="modal-text">${description}</p>
 
-            <div class="skill-button-container">
-              <ul class="work-list">
-                <li>${skills[0]}</li>
-                <li>${skills[1]}</li>
-                <li>${skills[2]}</li>
-              </ul>
+            <div class="skill-button">
+                <ul class="work-list">
+                  <li><a href=""> html</a></li>
+                  <li><a href=""> css</a></li>
+                  <li><a href=""> javaScript</a></li>
+               <li class="desktop_link"><a href=""> Ruby</a></li>
+               <li class="desktop_link"><a href=""> Bootstrap</a></li>
+                  </ul> <hr>
               <div class="button-wrapper">
-                  <button class="pop-up-btn btn">
-                  <span>See Live</span>
-                  <a href="#"><img src="./Images/Icon.png" alt=""></a>
+                  <button class="btn pop-up-btn "> <span>See Live</span><a href="#"><img src="./Images/Icon.png" alt=""></a>
                   </button>
-                  <button class="pop-up-btn btn">
-                  <span>See Source</span>
-                      <a href="#"> <img src="./Images/Vector.png" alt=""></a>
+                  <button class="btn pop-up-btn "><span>See Source</span><a href="#"> <img src="./Images/Vector.png" alt=""></a>
                   </button>
               </div>
           </div>
       </div>
     </section>
 `;
+
   return sampleTemplateModal;
 }
 
-const workCard = {
-  card1: {
-    id: 1,
-    title: 'Tonic',
-    image: './Images/1.png',
-    description: ` Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                   when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    skills: ['HTML', 'CSS', 'JavaScript'],
-  },
-
-  card2: {
-    id: 2,
-    title: 'Multi-Post Stories',
-    image: './Images/2.png',
-    description: ` Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                   when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                   It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    skills: ['HTML', 'CSS', 'JavaScript'],
-  },
-
-  card3: {
-    id: 3,
-    title: 'Tonic',
-    image: './Images/3.png',
-    description: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    skills: ['HTML', 'CSS', 'JavaScript'],
-  },
-
-  card4: {
-    id: 4,
-    title: 'Multi-Post Stories',
-    image: './Images/4.png',
-    description: `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
-    skills: ['HTML', 'CSS', 'JavaScript'],
-  },
-};
 function openModal(modal) {
   if (modal == null) return;
   modal.classList.add('active');
@@ -114,24 +152,16 @@ function closeModal(modal) {
 openPopButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
     const name = e.target.id;
-    const prj = Object.values(workCard)[name - 1];
+    const prj = works[name - 1];
     const modal = createModal(prj.id, prj.title,
-      prj.image, prj.description, prj.skills);
+      prj.image, prj.description1, prj.farme);
     const element = document.createElement('div');
     element.innerHTML = modal;
     element.classList.add('modal');
-    element.setAttribute('id', 'test');
-    const addElement = document.getElementById('work');
+    const addElement = document.getElementById('overlay');
     addElement.after(element);
-    element.after(overlay);
     openModal(element);
-    const figures = document.querySelectorAll('.project-showing figure');
-    figures.forEach((figure) => {
-      figure.style.width = '100%';
-      figure.style.marginTop = '2rem';
-    });
     const closePopButtons = document.querySelectorAll('.data-close-button');
-
     closePopButtons.forEach((button) => {
       button.addEventListener('click', () => {
         const modal = button.closest('.modal');
